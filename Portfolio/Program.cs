b3698ad5627809191d0b4b5841ba42721ef1fe35
genkokudo <y-toyoda@live.jp>
2026-06-30 01:01:29 +0900

    AzureFunctionsからデータ取得する

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient("functions", client =>
+{
+    var config = builder.Configuration;
+    client.BaseAddress = new Uri(config["AzureFunctions:BaseUrl"]!);
+});
+
+
 await builder.Build().RunAsync();
