d334f7d53604ca92d87b081f8455dba2eb4957e3
genkokudo <y-toyoda@live.jp>
2026-07-29 17:05:39 +0900

    HeroSectionをDBからの取得に変更

--- Portfolio.Shared/Model/PortfolioData.cs
+++ Portfolio.Shared/Model/PortfolioData.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Portfolio.Shared.Model;
 
-namespace Portfolio.Shared.Model
+public class PortfolioData
 {
-    public class PortfolioData
-    {
-        public string AuthorNameKanji { get; set; } = string.Empty;
-        public string AuthorNameHiragana { get; set; } = string.Empty;
-        public string AuthorNameAlphabet { get; set; } = string.Empty;
+    public string AuthorNameKanji { get; set; } = string.Empty;
+    public string AuthorNameHiragana { get; set; } = string.Empty;
+    public string AuthorNameAlphabet { get; set; } = string.Empty;
 
-        public string ProfileImageUrl { get; set; } = string.Empty;
-        public string ProfessionalTitle { get; set; } = string.Empty;
-        public string ProfileDescription { get; set; } = string.Empty;
-        public string GitHubUrl { get; set; } = string.Empty;
-        public List<string> ProfileTags { get; set; } = [];
+    public string ProfileImageUrl { get; set; } = string.Empty;
+    public string ProfessionalTitle { get; set; } = string.Empty;
+    public string HeroTitle { get; set; } = string.Empty;
+    public string ProfileDescription { get; set; } = string.Empty;
+    public string GitHubUrl { get; set; } = string.Empty;
+    public List<string> ProfileTags { get; set; } = [];
 
-        public List<SkillItem> Skills { get; set; } = [];
-        public List<WorkHistoryItem> WorkHistories { get; set; } = [];
-        public List<WorkItem> Works { get; set; } = [];
-    }
+    public List<SkillItem> Skills { get; set; } = [];
+    public List<WorkHistoryItem> WorkHistories { get; set; } = [];
+    public List<WorkItem> Works { get; set; } = [];
 }
+
--- Portfolio.Shared/Model/ProfileItem.cs
+++ Portfolio.Shared/Model/ProfileItem.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
+namespace Portfolio.Shared.Model;
 
-namespace Portfolio.Shared.Model
+public class ProfileItem
 {
-    public class ProfileItem
-    {
-        [JsonPropertyName("id")]
-        public string Id { get; set; } = string.Empty;
-        [JsonPropertyName("authorNameKanji")]
-        public string AuthorNameKanji { get; set; } = string.Empty;
-        [JsonPropertyName("authorNameHiragana")]
-        public string AuthorNameHiragana { get; set; } = string.Empty;
-        [JsonPropertyName("authorNameAlphabet")]
-        public string AuthorNameAlphabet { get; set; } = string.Empty;
-        [JsonPropertyName("profileImageUrl")]
-        public string ProfileImageUrl { get; set; } = string.Empty;
-        [JsonPropertyName("professionalTitle")]
-        public string ProfessionalTitle { get; set; } = string.Empty;
-        [JsonPropertyName("profileDescription")]
-        public string ProfileDescription { get; set; } = string.Empty;
-        [JsonPropertyName("gitHubUrl")]
-        public string GitHubUrl { get; set; } = string.Empty;
-        [JsonPropertyName("profileTags")]
-        public List<string> ProfileTags { get; set; } = new();
-    }
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = string.Empty;
+    [JsonPropertyName("authorNameKanji")]
+    public string AuthorNameKanji { get; set; } = string.Empty;
+    [JsonPropertyName("authorNameHiragana")]
+    public string AuthorNameHiragana { get; set; } = string.Empty;
+    [JsonPropertyName("authorNameAlphabet")]
+    public string AuthorNameAlphabet { get; set; } = string.Empty;
+    [JsonPropertyName("profileImageUrl")]
+    public string ProfileImageUrl { get; set; } = string.Empty;
+    [JsonPropertyName("professionalTitle")]
+    public string ProfessionalTitle { get; set; } = string.Empty;
+    [JsonPropertyName("heroTitle")]
+    public string HeroTitle { get; set; } = string.Empty;
+    [JsonPropertyName("profileDescription")]
+    public string ProfileDescription { get; set; } = string.Empty;
+    [JsonPropertyName("gitHubUrl")]
+    public string GitHubUrl { get; set; } = string.Empty;
+    [JsonPropertyName("profileTags")]
+    public List<string> ProfileTags { get; set; } = new();
 }
